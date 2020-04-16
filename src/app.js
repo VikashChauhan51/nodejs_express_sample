c2fb9327e81c8d77bbffbf3db026f615c84e200d
Vikash Chauhan <vikashchauhan51@gmail.com>
2020-04-16 13:37:47 +0530

    setup express-handlebars

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 const express = require('express');
-//instance of express
-const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
 //App port
 const PORT=8585;
+//instance of express
+const app = express();
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css',express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js',express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js',express.static(path.join(__dirname, '/node_modules/jquery/dist/js')));
+
+// Create `ExpressHandlebars` instance with a default layout.
+// const hbs = exphbs.create({
+// 	helpers: helpers,
+
+// 	// Uses multiple partials dirs, templates in "shared/templates/" are shared
+// 	// with the client-side of the app (see below).
+// 	partialsDir: [
+// 		"shared/templates/",
+// 		"views/partials/",
+//    ],
+   
+// });
 // This responds with "Hello World" on the homepage
 app.get('/',  (req, res)=> {
-   res.send('Hello GET');
+   res.render('home', {layout: false});
 });
  
 // Start server
